e837da07062746a49d50c4b3c5f9e3828af1502b
JellyBoonz <86168103+JellyBoonz@users.noreply.github.com>
2022-09-27 09:51:29 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,17 +1,32 @@
 public class Driver {
     public static void main(String[] args) {
         BinaryTree myTree = new BinaryTree(7);
+        BinaryTree notBST = new BinaryTree(8);
 
-        System.out.println("-----Before-----");
-        myTree.inOrderTreeWalk(myTree.root);
         System.out.println();
 
-        myTree.add(8);
-        myTree.add(4);
-        myTree.add(9);
-        myTree.add(6);
-        myTree.add(7);
-        myTree.add(10);
+        //Adding to binary tree
+        myTree.root.setRight(new TreeNode(10));
+        myTree.root.getRight().setLeft(new TreeNode(8));
+        myTree.root.getRight().setRight(new TreeNode(11));
+
+        myTree.root.setLeft(new TreeNode(4));
+        myTree.root.getLeft().setLeft(new TreeNode(3));
+        myTree.root.getLeft().setRight(new TreeNode(6));
+        myTree.root.getLeft().getRight().setLeft(new TreeNode(5));
+
+
+        //Adding to notBST
+        notBST.root.setRight(new TreeNode(10));
+        notBST.root.getRight().setLeft(new TreeNode(6));
+        notBST.root.getRight().setRight(new TreeNode(11));
+
+        notBST.root.setLeft(new TreeNode(4));
+        notBST.root.getLeft().setLeft(new TreeNode(3));
+        notBST.root.getLeft().setRight(new TreeNode(8));
+        notBST.root.getLeft().getRight().setLeft(new TreeNode(5));
+
+
 
         System.out.println("-----Inorder-----");
         myTree.inOrderTreeWalk(myTree.root);
@@ -27,5 +42,8 @@ public class Driver {
 
         System.out.print("Binary Tree? ");
         System.out.println(myTree.isBinaryTree(myTree.root));
+
+        System.out.print("Binary Tree? ");
+        System.out.println(notBST.isBinaryTree(notBST.root));
     }
 }
